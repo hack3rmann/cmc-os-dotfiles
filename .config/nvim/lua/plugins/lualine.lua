0141ef80dfa4e1006e3ef360bb357f5c281d4595
Svyatoslav <slava30727@github.com>
2024-09-13 12:06:25 +0000

    Lualine's theme changed to tokyonight

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
         options = {
-            theme = "horizon",
+            theme = "tokyonight",
             ignore_focus = { "neo-tree" },
         },
         sections = {
