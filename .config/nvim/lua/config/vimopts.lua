f0fffdb0c97004e6296eeb96c99f328dd1c5c8a3
Svyatoslav <slava30727@github.com>
2024-09-17 22:20:41 +0000

    Current number highlighter

--- .config/nvim/lua/config/vimopts.lua
+++ .config/nvim/lua/config/vimopts.lua
@@ -6,5 +6,8 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 8
 vim.opt.softtabstop = 0
 vim.opt.smartindent = false
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
+vim.cmd [[hi CursorLineNr guifg=#af00af]]
 
 vim.g.mapleader = " "
