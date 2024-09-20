0f13f32e31a05a7b7c0f1d7450872e706b9be86e
Svyatoslav <slava30727@github.com>
2024-09-20 18:53:52 +0000

    Convinient rust installation

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 # Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs --output rustup.sh
+sh rustup.sh -y
+rm rustup.sh
 
 source ~/.bashrc
 
 # Install tools
 cargo install zoxide bat eza
+
+source ~/.bashrc
