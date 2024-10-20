63b91910e81e0883081cf72870ffd26b83d8ffdc
Svyatoslav <slava30727@github.com>
2024-10-20 15:06:58 +0000

    Better `.bashrc` initialization

--- .aliases.sh
+++ .aliases.sh
@@ -1,17 +1,67 @@
-alias ls="eza"
-alias la="eza -a"
-alias lal="eza -al"
-alias tree="eza -aT"
+# ENVs
+. "$HOME/.cargo/env"
+PATH="$PATH:$HOME/.cargo/"
+
+# `eza` is installed
+if [[ ! "$(which eza)" = "" ]]; then
+    alias ls="eza"
+    alias la="eza -a"
+    alias lal="eza -al"
+    alias tree="eza -T"
+fi
+
+# `bat` is installed
+if [[ ! "$(which bat)" = "" ]]; then
+    alias bat="bat --theme TwoDark"
+fi
 
 DEFAULT_CC_FLAGS="-Wall -Wextra -std=gnu23 -lm"
 
 alias gccfg="gcc ${DEFAULT_CC_FLAGS} -g"
 alias gccfo="gcc ${DEFAULT_CC_FLAGS} -O2"
 
-alias bat="bat --theme TwoDark"
+# Custom compile commands
 
-alias vim="nvim"
-alias vi="nvim"
-. "$HOME/.cargo/env"
+ccc() {
+    # Create compiled binary directory
+    mkdir -p bin
+    
+    FILE_NAME_REGEX='^(.*)\.(c|cpp|h|hpp)$'
 
-PATH="$PATH:$HOME/.cargo/"
+    if [[ "$2" =~ $FILE_NAME_REGEX ]]; then
+        FILE_NAME=${BASH_REMATCH[1]}
+        EXTENSION=${BASH_REMATCH[2]}
+
+        if [[ ! $EXTENSION = "c" ]]; then
+            FILE_NAME=$2
+            printf "Unsupported extension '$EXTENSION'\n"
+        fi
+    else
+        echo DOESN\'T MATCH
+        FILE_NAME=$2
+    fi
+
+    gcc $DEFAULT_CC_FLAGS $1 $2 -o bin/$FILE_NAME
+}
+
+alias ccg="ccc -g"
+alias cco="ccc -O2"
+
+CONTEST_PATH="$HOME/contest"
+ATTACH_SCRIPT="$HOME/dotfiles/attach_ejudge.sh"
+
+submit() {
+    if [[ $1 = "--help" || $1 = "-h" ]]; then
+        printf "ejudge solution submition helper.\n\nUsage: submit [PROBLEM] [FILE]\n\n"
+        return 1
+    fi
+
+    # Attach ejudge if it is not attached
+    if [[ ! -d $CONTEST_PATH ]] || [[ -z "$(ls -A $CONTEST_PATH)" ]]; then
+        echo "Authorize ejudge:"
+        $ATTACH_SCRIPT
+    fi
+
+    echo "Submitting solution $2 to problem $1"
+    cp "$2" "$CONTEST_PATH/524/problems/$1/submit/gcc/"
+}
--- .bashrc
+++ .bashrc
@@ -11,9 +11,6 @@ if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
 fi
 export PATH
 
-# Uncomment the following line if you don't like systemctl's auto-paging feature:
-# export SYSTEMD_PAGER=
-
 # User specific aliases and functions
 if [ -d ~/.bashrc.d ]; then
     for rc in ~/.bashrc.d/*; do
@@ -24,26 +21,7 @@ if [ -d ~/.bashrc.d ]; then
 fi
 unset rc
 
-ZSH="$HOME/.local/bin/zsh"
-if [[ -x $ZSH ]]; then
-    $ZSH
-fi
-
 source ~/.aliases.sh
 
 eval "$(zoxide init bash --cmd cd)"
 . "$HOME/.cargo/env"
-
-CONTEST_PATH="$HOME/contest"
-ATTACH_SCRIPT="$HOME/dotfiles/attach_ejudge.sh"
-
-submit() {
-    # Attach ejudge if it is not attached
-    if [[ ! -d $CONTEST_PATH ]] || [[ -z "$(ls -A $CONTEST_PATH)" ]]; then
-        echo "Authorize ejudge:"
-        $ATTACH_SCRIPT
-    fi
-
-    echo "Submitting solution $2 to problem $1"
-    cp "$2" "$CONTEST_PATH/524/problems/$1/submit/gcc/"
-}
